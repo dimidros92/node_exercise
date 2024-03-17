71c4276ddeaf3f10ead307ad8561217df7f166aa
dimidros <jimmydrososs@gmail.com>
2024-03-17 17:44:37 +0200

    Message model
    message.js model sequelize for db table

---
+++ models/Message.js
@@ -0,0 +1,35 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../db_config");
+
+const Message = sequelize.define("Message", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sender: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  receiver: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  seen: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
+  },
+  timestampSent: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+});
+
+module.exports = Message;
