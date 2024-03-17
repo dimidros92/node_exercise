8c887721a0cbf32aef112b20dae5867e713bd9c1
dimidros <jimmydrososs@gmail.com>
2024-03-17 17:13:27 +0200

    db config
    init push and database configuration with dotenv as hinted

--- Server.js
+++ Server.js
@@ -1,6 +1,15 @@
-const http = require('http');
-const app = require('./App');
+const http = require("http");
+const app = require("./App");
 
+const sequelize = require("./db_config");
 const port = 3000;
-const server = http.createServer(app);
-server.listen(port);
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection with database has been established successfully.");
+    const server = http.createServer(app);
+    server.listen(port);
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
