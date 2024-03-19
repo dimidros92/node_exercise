f190743a277367dcdbce7ded5367ea74184d962c
dimidros <jimmydrososs@gmail.com>
2024-03-19 10:32:24 +0200

    query for last interactions for one user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
+const sequelize = require("../db_config");
+const Message = require("../models/Message");
 const User = require("../models/User");
-const { Op, fn, col } = require("sequelize");
+const { Op, QueryTypes } = require("sequelize");
 
 /**
  * Get users by predicate
@@ -41,7 +43,50 @@ function byPredicate(predicate) {
   }
   return User.findAll({ where });
 }
+
+/**
+ * Get users that have had a conversation with userId
+ *  @param {number} userId
+ */
+function conversedWith(userId) {
+  return sequelize.query(
+    `SELECT u.id,
+              u.first_name,
+              u.last_name,
+              u.birthday,
+              u.gender,
+              u.username,
+              MAX(latest_interaction) AS latest_interaction
+       FROM users u
+                JOIN (SELECT sender              AS user_id,
+                             MAX(timestamp_sent) AS latest_interaction
+                      FROM messages
+                      WHERE receiver = :userId
+                      GROUP BY sender
+                      UNION ALL
+                      SELECT receiver            AS user_id,
+                             MAX(timestamp_sent) AS latest_interaction
+                      FROM messages
+                      WHERE sender = :userId
+                      GROUP BY receiver) AS interactions ON u.id = interactions.user_id
+       GROUP BY u.id
+       ORDER BY MAX(latest_interaction) DESC`,
+    {
+      replacements: { userId: userId },
+      type: QueryTypes.SELECT,
+      model: User,
+      mapToModel: true,
+    },
+  );
+}
+
 exports.byPredicate = async (req, res) => {
   const users = await byPredicate(req.query);
+  res.status(200).json(users);
+};
+
+exports.conversedWith = async (req, res) => {
+  const users = await conversedWith(req.params.userId);
+  console.log(users);
   res.status(200).json(users);
 };
