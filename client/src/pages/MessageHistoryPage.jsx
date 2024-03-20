38681f2805e66cadbd89b6220dea93e2fb8241a9
dimidros <jimmydrososs@gmail.com>
2024-03-20 23:32:15 +0200

    select test

--- client/src/pages/MessageHistoryPage.jsx
+++ client/src/pages/MessageHistoryPage.jsx
@@ -1,12 +1,20 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../state/users/userSlice";
 
 export const MessageHistoryPage = () => {
   const dispatch = useDispatch();
+  const users = useSelector((state) => state.users);
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return <div></div>;
+  return (
+    <div>
+      {users.loading && <h4>Loading ...</h4>}
+      {users &&
+        users.users.length > 0 &&
+        users.users.map((user) => <h4>{user.firstName}</h4>)}
+    </div>
+  );
 };
