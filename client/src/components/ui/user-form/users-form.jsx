1796c74eab61a2b0cab30438fe2d55a6be3f1ca7
dimidros <jimmydrososs@gmail.com>
2024-03-22 00:11:36 +0200

    user form component

---
+++ client/src/components/ui/user-form/users-form.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import "./user-form.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import UserSelect from "../user-select";
+
+export default function UsersForm({ users, selected }) {
+  const [userA, setUserA] = React.useState(undefined);
+  const [userB, setUserB] = React.useState(undefined);
+  React.useEffect(() => {
+    if (userA && userB) {
+      selected([userA, userB]);
+    }
+  }, [userA, userB, selected]);
+  return (
+    <Card sx={{ minWidth: 345 }}>
+      <CardContent className="card">
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Choose two users to see their conversation
+        </Typography>
+        <UserSelect
+          label="User A"
+          selected={setUserA}
+          users={users.filter((user) => user.id !== userB?.id ?? -1)}
+        ></UserSelect>
+        <UserSelect
+          label="User B"
+          selected={setUserB}
+          users={users.filter((user) => user.id !== userA?.id ?? -1)}
+        ></UserSelect>
+      </CardContent>
+    </Card>
+  );
+}
