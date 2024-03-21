ec07429a277d52256922c21bf83a3fa0f0b15091
dimidros <jimmydrososs@gmail.com>
2024-03-22 00:12:20 +0200

    message history page

--- client/src/pages/MessageHistoryPage.jsx
+++ client/src/pages/MessageHistoryPage.jsx
@@ -1,20 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import "./MessageHistory.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../state/users/userSlice";
-
+import UsersForm from "../components/ui/user-form/users-form";
+import { fetchMessages } from "../state/messages/messageSlice";
+import MessageHistory from "../components/ui/message-history/message-history";
 export const MessageHistoryPage = () => {
   const dispatch = useDispatch();
-  const users = useSelector((state) => state.users);
+  const usersSlice = useSelector((state) => state.users);
+  const messagesSlice = useSelector((state) => state.messages);
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    if (users.length === 2) {
+      dispatch(fetchMessages(users));
+    }
+  }, [users, dispatch]);
   return (
-    <div>
-      {users.loading && <h4>Loading ...</h4>}
-      {users &&
-        users.users.length > 0 &&
-        users.users.map((user) => <h4>{user.firstName}</h4>)}
+    <div className="page">
+      <UsersForm selected={setUsers} users={usersSlice.users} />
+      {messagesSlice.messages.length > 0 && users.length > 0 && (
+        <MessageHistory users={users} messages={messagesSlice.messages} />
+      )}
     </div>
   );
 };
