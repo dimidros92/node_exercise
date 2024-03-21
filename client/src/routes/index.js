11aa26cfcfb76eba2a19310e934104c017ecb2e5
dimidros <jimmydrososs@gmail.com>
2024-03-22 00:13:07 +0200

    new packages added for components

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import { MessageHistoryPage } from "../pages/MessageHistoryPage";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <MessageHistoryPage /> },
+]);
 
 export default router;
