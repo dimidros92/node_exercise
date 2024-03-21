6240c0815aa68185d1682fc903da096b949c2466
dimidros <jimmydrososs@gmail.com>
2024-03-22 00:12:08 +0200

    user select component

---
+++ client/src/components/ui/user-select.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function UserSelect({ label, users, selected }) {
+  const [userId, setUserId] = React.useState("");
+
+  const handleChange = (event) => {
+    setUserId(event.target.value);
+    selected(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 220 }}>
+      <FormControl fullWidth>
+        <InputLabel id="select-label">{label}</InputLabel>
+        <Select
+          labelId="select-label"
+          id="select"
+          value={userId}
+          label={label}
+          onChange={handleChange}
+        >
+          {users &&
+            users.length > 0 &&
+            users.map((user) => (
+              <MenuItem key={user.id} value={user}>
+                {user.firstName + " " + user.lastName}
+              </MenuItem>
+            ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
