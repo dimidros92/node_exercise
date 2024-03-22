99cf38c1609a4e94080475134fef94bc2a8c096e
dimidros <jimmydrososs@gmail.com>
2024-03-22 23:42:25 +0200

    removed unused App component

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import { MessageHistoryPage } from "../pages/MessageHistoryPage";
 
 const router = createBrowserRouter([
